5c5c9116acd6aabad8729f453fba6f1a66cd0065
N <hatarakuotamasan8@outlook.jp>
2023-09-06 13:45:13 +0900

    create post page

---
+++ notion-blog/src/pages/posts/[slug].tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <section className="container lg:px-2 px-5 mx-auto mt-20 lg:w-2/5 ">
+      <h2 className="w-full text-2xl font-medium">3回目の投稿です</h2>
+      <div className="border-b-2 w-1/3 mt-1 border-sky-900"></div>
+      <span className="text-gray-500">2023/09/06</span>
+      <br />
+      <p className="bg-sky-900 text-white rounded-xl font-medium px-2 mt-2 inline-block">
+        Next.js13
+      </p>
+      <div className="mt-10 font-medium">dxzlfgjasdfljasfasolfjal</div>
+    </section>
+  );
+};
+
+export default Post;
